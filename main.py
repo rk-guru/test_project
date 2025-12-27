cef44c17d97a67050bd178b68588b31d21d2fdb8
Kumaraguru R <r.kumaraguru44@gmail.com>
2025-12-27 12:39:01 +0530

    updated_2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from rage_fun import ragg
 
 rag_1=ragg()
-resp=rag_1.invoke('what is the inlet temperature for the styrene production process')
+resp=rag_1.invoke('what is the inlet temperature for the styrene production process , what is the pressure ')
 print(resp)
 
 
